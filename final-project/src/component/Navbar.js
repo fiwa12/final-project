ef387d499d976409262d2592b4143803add144fe
Gedearya <igd.ad.pratama@gmail.com>
2021-01-26 02:49:38 +0700

    update img and navbar js css

--- final-project/src/component/Navbar.js
+++ final-project/src/component/Navbar.js
@@ -1,92 +1,122 @@
 import React, { Component } from "react";
+import "./../style/Navbar.css";
 
 class Navbar extends Component {
   render() {
     return (
       <div>
-        <header>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
           <div className="container">
-            {/*/nav*/}
-            <nav className="navbar navbar-expand-lg navbar-light fill px-lg-0 py-0 px-sm-3 px-0">
-              <a className="navbar-brand" href="index.html">
-                <span className="fab fa-pinterest" />
-              </a>
-              <ul className="navbar-nav">
+            <a className="navbar-brand font-weight-bold" href="#">
+              <img
+                src="img/pinterest-logo.png"
+                width={55}
+                height={30}
+                className="d-inline-block align-top"
+                alt=""
+              />
+              PINSTAGRAM
+            </a>
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
-                  <a className="nav-link" href="index.html">
-                    Home
+                  <a className="nav-link" href="#">
+                    Home <span className="sr-only">(current)</span>
                   </a>
                 </li>
               </ul>
-              <div
-                className="collapse navbar-collapse"
-                id="navbarSupportedContent"
-              >
-                <nav className="mx-auto">
-                  <div className="search-bar">
-                    <form className="search">
-                      <input
-                        type="search"
-                        className="search__input"
-                        name="search"
-                        placeholder="Search other styles"
-                        onload="equalWidth()"
-                        required
-                      />
-                      <span className="fa fa-search search__icon" />
-                    </form>
-                  </div>
-                </nav>
-                <ul className="navbar-nav">
-                  {/* notification bell */}
-                  <li className="nav-item active">
-                    <a className="nav-link" href="index.html">
-                      <i className="fa fa-bell">
-                        <span class="badge badge-pill badge-danger">2</span>
-                      </i>
-                    </a>
-                  </li>
-                  <li className="nav-item active">
-                    <a className="nav-link" href="index.html">
-                      <span className="fas fa-plus-circle" />
-                    </a>
-                  </li>
-                  <li className="nav-item dropdown @@pages__active">
-                    <a
-                      className="nav-link dropdown-toggle"
-                      href="#"
-                      id="navbarDropdown"
-                      role="button"
-                      data-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      <span className="fa fa-user-circle" />
-                    </a>
-                    <div
-                      className="dropdown-menu"
-                      aria-labelledby="navbarDropdown"
-                    >
-                      <a
-                        className="dropdown-item @@b__active"
-                        href=".html"
+              <form action="">
+                <div className="p-1 bg-light rounded rounded-pill shadow-sm">
+                  <div className="input-group">
+                    <div className="input-group-prepend">
+                      <button
+                        id="button-addon2"
+                        type="submit"
+                        className="btn btn-link text-secondary"
                       >
-                        Settings
-                      </a>
-                      <a
-                        className="dropdown-item @@fa__active"
-                        href=".html"
-                      >
-                        Log Out
-                      </a>
+                        <i className="fas fa-search" />
+                      </button>
                     </div>
-                  </li>
-                </ul>
-              </div>
-            </nav>
+                    <input
+                      type="search"
+                      placeholder="Search your style?"
+                      aria-describedby="button-addon2"
+                      className="form-control border-0 bg-light"
+                    />
+                  </div>
+                </div>
+              </form>
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <h4>
+                      <i class="fas fa-bell ml-3 mr-3"></i>
+                    </h4>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <h4>
+                      <i class="fas fa-comment-dots mr-3"></i>
+                    </h4>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <h4>
+                      <i class="fas fa-plus-circle mr-3"></i>
+                    </h4>
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <span className="profile-picture">
+                      <img src="img/Arya.jpg" alt="" />
+                    </span>
+                  </a>
+                  <div
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    <a className="dropdown-item" href="#">
+                      Manage Account
+                    </a>
+                    <a className="dropdown-item" href="#">
+                      Setting
+                    </a>
+                    <div className="dropdown-divider" />
+                    <a className="dropdown-item" href="#">
+                      Sign out
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
-          {/*//nav*/}
-        </header>
+        </nav>
       </div>
     );
   }
