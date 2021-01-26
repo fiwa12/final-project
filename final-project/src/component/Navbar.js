f4691b3fcc92bfd8ec4407348c486b997a964b72
Gedearya <igd.ad.pratama@gmail.com>
2021-01-26 13:11:13 +0700

    Update navbar.js

--- final-project/src/component/Navbar.js
+++ final-project/src/component/Navbar.js
@@ -39,6 +39,11 @@ class Navbar extends Component {
                     Home <span className="sr-only">(current)</span>
                   </a>
                 </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Today
+                  </a>
+                </li>
               </ul>
               <form action="">
                 <div className="p-1 bg-light rounded rounded-pill shadow-sm">
