a7c8d1b8a40d04f910d28cba03f00217ce31611e
fira <firaadani@gmail.com>
2021-01-26 19:40:27 +0700

    Back to content only

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
-import ChipsItem from "./component/ChipsItem";
-import HorizontalScrollMenu from "./component/HorizontalScrollMenu";
+// import ChipsItem from "./component/ChipsItem";
+// import HorizontalScrollMenu from "./component/HorizontalScrollMenu";
 import Content from "./component/Content";
+// import Navbar from "./component/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <Navbar />  */}
-        <HorizontalScrollMenu />
-        <ChipsItem />
+        {/* <Navbar /> */}
+        {/* <HorizontalScrollMenu /> */}
+        {/* <ChipsItem /> */}
         <Content />
       </div>
     );
