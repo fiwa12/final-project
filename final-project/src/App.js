f62affa32172c88765b1c61c666247e5800f5516
nijiko <nijikos@gmail.com>
2021-01-26 19:07:41 +0700

    Fixed Missing Folders

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import TagFilterBar from "./component/TagFilterBar";
-import Content from "./component/Content";
-import Navbar from "./component/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
         <TagFilterBar />
-        <Content />
       </div>
     );
   }
